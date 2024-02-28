3d9c43970b24c1ac46fdb9134c1d7c0341102548
klau55 <kipatok@bk.ru>
2024-02-28 18:57:23 +0200

    added img struct

--- window_test.c
+++ window_test.c
@@ -6,7 +6,7 @@
 /*   By: nkarpilo <nkarpilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 13:44:33 by nkarpilo          #+#    #+#             */
-/*   Updated: 2024/02/28 16:51:13 by nkarpilo         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:48:36 by nkarpilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,32 +24,21 @@
 #define WIDTH 1024
 #define HEIGHT 768
 
-static mlx_image_t* image;
-static mlx_texture_t* texture;
+
 
 // -----------------------------------------------------------------------------
+
+typedef struct s_img
+{
+	mlx_texture_t	*txt_pl;
+	mlx_image_t		*img_pl;
+}	t_img;
+
+static t_img* img;
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
     return (r << 24 | g << 16 | b << 8 | a);
-}
-
-void ft_randomize(void* param)
-{
-	(void)param;
-	for (uint32_t i = 0; i < image->width; ++i)
-	{
-		for (uint32_t y = 0; y < image->height; ++y)
-		{
-			uint32_t color = ft_pixel(
-				rand() % 0xFF, // R
-				rand() % 0xFF, // G
-				rand() % 0xFF, // B
-				rand() % 0xFF  // A
-			);
-			mlx_put_pixel(image, i, y, color);
-		}
-	}
 }
 
 void ft_hook(void* param)
@@ -59,47 +48,66 @@ void ft_hook(void* param)
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
+		img->img_pl->instances[0].y -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
+		img->img_pl->instances[0].y += 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
+		img->img_pl->instances[0].x -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
+		img->img_pl->instances[0].x += 5;
 }
-
-
-int32_t main(void)
+/*
+int	render_player(mlx_t *mlx, mlx_image_t *image, mlx_texture_t *texture)
 {
-	mlx_t* mlx;
-
-	// Gotta error check this stuff
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-	{
-		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
-	}
 	texture = mlx_load_png("assets/monky.png");
 	if (!texture)
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	image = mlx_texture_to_image(mlx, texture);
 	if (!image)
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-	
 	mlx_resize_image(image, 64, 128);
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	return (0);
+} */
+
+
+int32_t	main(void)
+{
+	mlx_t			*mlx;
+
+	img = (t_img *)malloc(sizeof(t_img));
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "solo ng", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
+	img->txt_pl = mlx_load_png("assets/monky.png");
+	if (!img->txt_pl)
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
+	}
+	img->img_pl = mlx_texture_to_image(mlx, img->txt_pl);
+	if (!img->img_pl)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
+	mlx_resize_image(img->img_pl, 64, 128);
+	if (mlx_image_to_window(mlx, img->img_pl, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
 	}
 	mlx_loop_hook(mlx, ft_hook, mlx);
 
